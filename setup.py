eb6528f5714ddaafb22bd0ab94346c25ed082555
arristigit <arristimedia@gmail.com>
2024-09-19 03:02:22 +0530

    Corrected package name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-    name="secureai",
+    name="securedai",
     version="0.1.0",
-    description="Secured you GenAI models using securedai",
+    description="Secured your GenAI models using securedai",
     long_description="This is very long description for securedai",
-    author="Varinder CTO",
+    author="Varinder Kumar",
     packages=["securedai"],
     install_requires=["python-decouple==3.8"]
 )
