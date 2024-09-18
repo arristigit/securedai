a5c5cf22136d340af80d5cff9866aef7b2767402
arristigit <arristimedia@gmail.com>
2024-09-19 02:50:40 +0530

    Created, build & installed securedai

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="secureai",
+    version="0.1.0",
+    description="Secured you GenAI models using securedai",
+    long_description="This is very long description for securedai",
+    author="Varinder CTO",
+    packages=["securedai"],
+    install_requires=["python-decouple==3.8"]
+)
