1201021aa6e1d30ad4e31537a55ed8f420121a93
arristigit <arristimedia@gmail.com>
2024-09-19 04:24:41 +0530

    README file improved with v0.1.1

--- build/lib/securedai/__init__.py
+++ build/lib/securedai/__init__.py
@@ -2,6 +2,6 @@ class Secured():
     def __init__(self) -> None:
         print("Constructed secured instance")
     
-    def implement(self, num):
-        print(f"Implemented securety for {num} models!")
+    def implement(self, model_count: int) -> dict:
+        print(f"Implemented securety for {model_count} models!")
         return {"status": True, "message": "Success"}
--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name="securedai",
-    version="0.1.0",
-    description="Secured your GenAI models using securedai",
-    long_description="This is very long description for securedai",
+    version="0.1.1",
+    description="Secure your ML models using securedai",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/arristigit/securedai",
     author="Varinder Kumar",
-    packages=["securedai"],
+    packages=find_packages(),
     install_requires=["python-decouple==3.8"]
 )
